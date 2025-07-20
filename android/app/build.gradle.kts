b09c28a826c13b45d1a9abbda74c7ce4a55cee23
NishantMeena <nishantmeena89@gmail.com>
2025-07-20 18:23:14 +0530

    launcher icon changed

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.eco.trail.ecotrail"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 5
-        versionName = "1.0.4"
+        versionCode = 6
+        versionName = "1.0.5"
     }
 
     signingConfigs {
